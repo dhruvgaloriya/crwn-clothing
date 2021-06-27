02299b81ad52a7ddbae71c505c3cef1f0ad8b13d
Dhruvit Galoriya <dhruvitgaloriya@gofynd.com>
2021-06-27 14:55:57 +0530

    add sign in component and page

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import {Switch, Route} from 'react-router-dom'
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
-import './App.css';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
+
+import './App.css';
 function App() {
   return (
-    <div>
+    <>
       <Header/>
       <Switch>
         <Route exact path={"/"} component={HomePage}/>
         <Route path='/shop' component={ShopPage} />
+        <Route path="/sign-in" component={SignInAndSignUpPage} />
       </Switch>
-    </div>
+    </>
   );
 }
 
