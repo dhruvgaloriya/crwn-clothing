e355753cb9dbe28c78b030081495cd749a638d98
Dhruvit Galoriya <dhruvitgaloriya@gofynd.com>
2021-07-18 19:50:05 +0530

    display data in cart and add functionality of add to cart button

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
-import { connect } from 'react-redux';
-import {ReactComponent as ShoppingIcon} from "../../assets/shopping-bag.svg";
-import { toggleCartHidden } from '../../redux/cart/cart.action';
-import "./cart-icon.styles.scss"
-const CartIcon = ({toggleCart}) => (
-  <div className="cart-icon" onClick={toggleCart}>
-    <ShoppingIcon className="shopping-icon"/>
-    <span className="item-count">0</span>
-  </div>
-)
+import React from "react";
+import { connect } from "react-redux";
+import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
+import { toggleCartHidden } from "../../redux/cart/cart.action";
+import "./cart-icon.styles.scss";
 
+const CartIcon = ({ toggleCart, itemCount }) => (
+  <div className="cart-icon" onClick={toggleCart}>
+    <ShoppingIcon className="shopping-icon" />
+    <span className="item-count">{itemCount}</span>
+  </div>
+);
 
 const mapDispatchToProps = (dispatch) => ({
-  toggleCart: () => dispatch(toggleCartHidden())
-})
+  toggleCart: () => dispatch(toggleCartHidden()),
+});
 
-export default connect(null, mapDispatchToProps)(CartIcon)
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  itemCount: cartItems.reduce((acc, cartItem) => acc + cartItem.quantity, 0),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
